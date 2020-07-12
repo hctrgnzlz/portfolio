bf3cd44f8d53ae2d901b9e722215a8db6ff0001f
hctrgnzlz <hectorgzz89@gmail.com>
2020-07-12 16:17:51 -0400

    variables, javascript, jquery added

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+//javascript//
+
 hamburgerMenu = function () {
   const menu = document.getElementById("nav__list");
-  const closeIcon = document.getElementById("toggle");
-
-  menu.addEventListener("click", handleMenuClick);
+  const close = document.getElementById("toggle");
 
   function handleMenuClick(event) {
     if (event.target instanceof HTMLAnchorElement) {
-      closeIcon.checked = false;
+      close.checked = false;
     }
   }
+  menu.addEventListener("click", handleMenuClick);
 };
+hamburgerMenu();
 
+//jquery//
 $(".eye").click(function () {
   $(this).addClass("active");
   $(".secret").fadeIn(300).addClass("active");
